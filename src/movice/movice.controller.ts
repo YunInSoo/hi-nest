664cc801bc1a887e337422c2a9df5903c260912c
yuninsoo <yunis@wecompany.kr>
2020-10-27 18:28:44 +0900

    Add controller in nestJS

--- src/movice/movice.controller.ts
+++ src/movice/movice.controller.ts
@@ -1,4 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('movice')
-export class MoviceController {}
+export class MoviceController {
+  @Get()
+  getAll(): string {
+    return 'all';
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): string {
+    return `hi ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData: string): string {
+    return `hi ${movieData}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number): string {
+    return `hi ${movieId}`;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: object): string {
+    return `hi ${movieId}`;
+  }
+}
